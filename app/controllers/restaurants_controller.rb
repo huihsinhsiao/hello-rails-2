593764bcb608baafc108c9381e09a61f9ad9fd40
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-03-29 17:52:48 +0800

    新增餐廳輸入欄位&頁面

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def list
+    # 寫入表單
+    # redirect 轉址 -> ex.回列表頁
+    redirect_to '/restaurants'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # get '/restaurants', to: 'a#b'
   get '/restaurants', to: 'restaurants#index'
+  get '/new_restaurant', to: 'restaurants#new'
+  post '/restaurant_list', to: 'restaurants#list'
 end
