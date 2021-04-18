a7e669252b394a436db03450e8b78574052206e3
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-04-18 16:33:17 +0800

    練習migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_062027) do
+ActiveRecord::Schema.define(version: 2021_04_18_081933) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "title"
